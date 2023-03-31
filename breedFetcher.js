53d5af7cddede064b34588d20ecda7365e5b2d17
B-Waterman <brooke.waterman@outlook.com>
2023-03-31 01:32:46 -0700

    implemented edge cases: request failed & breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,9 +4,12 @@ const breed = process.argv[2]; //cmd line arg name of desired breed
 const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
 request(url, (error, response, body) => { //start with body
+  if (error) { //request failed ex. typo in url
+    console.log("Error: request failed", error);
+  }
   const data = JSON.parse(body);
   const breedFound = data[0];
-  if (breedFound) {
-    console.log(breedFound.description);
-  }
+  if (!breedFound) {
+    return console.log(`${breed} not found. Please check spelling; if spelling correct, please ensure existence (wishful thinking unsupported at this time)`);
+  } console.log(breedFound.description);
 });
