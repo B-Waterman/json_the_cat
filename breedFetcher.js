43524d754316ba345b2ae58a5bfda12c54d7642c
B-Waterman <brooke.waterman@outlook.com>
2023-03-31 00:28:59 -0700

    Created breedFetcher.js. required 'request' library, declared url variable with api that searches for cat breeds, and began implementing fetching function

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,6 +2,15 @@ const request = require('request');
 
 const fetchBreedDescription = function(breed, callback) {
 
-  const url = `https://api.thecatapi.com/v1/breeds/search?q=`
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+  request(url, (error, response, body) => { //start with body
+    
+    const data = JSON.parse(body);
+    console.log(data);
+    console.log(typeof data);
+  })
 
 };
+
+console.log(typeof body);
